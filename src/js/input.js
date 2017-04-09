10ee479836c368afa635226b9eaa5c87bc975a2c
unscript <alexkh34@gmail.com>
2017-04-10 00:14:02 +0300

    add check input functions

--- src/js/input.js
+++ src/js/input.js
@@ -1,10 +1,14 @@
 var RT = RT || {};
 
 RT.input = (function () {
-    var startDay, startMonth, startYear, startHours, startMin, targetHours, targetMin;
+    var startDay, startMonth, startYear, startHours, startMin, targetHours, targetMin, inputForm;
+
+    var inputs = null;
 
     function init() {
         var date = new Date;
+        inputForm = RT.core.getElem("input-form");
+
         startDay = RT.core.getElem("start-day");
         startMonth = RT.core.getElem("start-month");
         startYear = RT.core.getElem("start-year");
@@ -22,9 +26,59 @@ RT.input = (function () {
         targetHours.value = 67;
         targetMin.value = 0;
 
-        targetMin.addEventListener("focus", function () {
-            targetMin.value = "";
-        }, false)
+        inputs = [startDay, startMonth, startYear, startHours, startMin, targetHours, targetMin];
+
+        inputForm.onsubmit = function (e) {
+            e.preventDefault();
+        }
+
+        // назначить обработчики
+        for (var i = 0; i < inputs.length; i++) {
+            inputs[i].oninput = checkInput;
+            inputs[i].onchange = validate;
+        }
+
+        toReadable();
+    }
+
+    function setDate() {
+
+    }
+
+    function toReadable() {
+        for (var i = 0; i < inputs.length; i++) {
+            _toReadable(inputs[i]);
+        }
+    }
+
+    function getDate() {
+
+    }
+
+
+    function checkInput() {
+        this.value = this.value.replace(/\D/gim, "");
+        console.log("check");
+    }
+
+    function validate() {
+        var min = parseInt(this.getAttribute("data-min"));
+        var max = parseInt(this.getAttribute("data-max"));
+        var value = parseInt(this.value);
+        if (value < min || isNaN(value)) {
+            this.value = min;
+        } else if (this.value > max) {
+            this.value = max;
+        }
+        _toReadable(this);
+    }
+
+    function _toReadable (input) {
+        var min = parseInt(input.getAttribute("data-auto"));
+
+        if (input.value < 10) {
+            input.value = "0" + input.value;
+        }
     }
 
     return {
